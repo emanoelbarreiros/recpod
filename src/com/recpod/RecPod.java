66ab836ddcbea0dfc9be7c0b3f896958303606a3
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2010-11-12 21:52:46 -0300

    testing second commit

--- src/com/recpod/RecPod.java
+++ src/com/recpod/RecPod.java
@@ -3,6 +3,7 @@ package com.recpod;
 import android.app.Activity;
 import android.os.Bundle;
 
+//Test
 public class RecPod extends Activity {
     /** Called when the activity is first created. */
     @Override
